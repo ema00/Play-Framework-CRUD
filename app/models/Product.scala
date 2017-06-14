4a814bb0afa6164c98f634a4ebbab72a1d195b02
Emanuel Aguirre <ema_a_@hotmail.com>
2017-06-13 22:58:14 -0300

    added comments to Product model

--- app/models/Product.scala
+++ app/models/Product.scala
@@ -1,10 +1,22 @@
 package models
 
 
+/**
+  * case class Product
+  * A simple model of a Product.
+  */
 case class Product(ean: Long, name: String, description: String)
 
 
+/**
+  * object Product
+  * Companion object for the Product case class.
+  */
 object Product {
+
+  /**
+  * Set containing the instances of Product.
+  */
   var products = Set(
     Product(5010255079763L, "Paperclips Large",
         "Large Plain Pack of 1000"),
@@ -18,20 +30,33 @@ object Product {
         "Zebra Length 28mm Assorted 150 Pack")
   )
 
+  /**
+    * findAll: Returns all the products.
+    */
   def findAll = products.toList.sortBy(_.ean)
 
+  /**
+    * findByEan: Returns an Option[Product] containing the product with the ean
+    * specified in the parameter.
+    */
   def findByEan(ean: Long) = products.find(p => p.ean == ean)
 
+  /**
+    * add: Adds a product to products.
+    */
   def add(product: Product) = { products = products + product }
 
+  /**
+    * remove: Removes a product from products.
+    */
   def remove(product: Product) = { products = products - product }
 
-  /*
-  * Updates product.
-  * Postcondition: Returns an Option, Some[Product] updated product in case
-  * product exists, or None in case product doesn't exist. Doesn't create a new
-  * product if product doesn't exist previously.
-  */
+  /**
+    * update: Updates product.
+    * Postcondition: Returns an Option, Some[Product] updated product in case
+    * product exists, or None in case product doesn't exist. Doesn't create a new
+    * product if product doesn't exist previously.
+    */
   def update(product: Product) = {
     products.find(p => p.ean == product.ean) match {
       case None => None
